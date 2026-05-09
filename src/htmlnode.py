14f52f48c15d902d3f661e286900c94ff63d2f8a
Dmitry Zverev <zda6694@proton.me>
2026-05-09 20:48:50 +0200

    ParentNode implementation

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -48,4 +48,13 @@ class LeafNode(HTMLNode):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
     
 
+class ParentNode(HTMLNode):
+    def __init__(self,tag = None, children = None, props = None):
+        super().__init__(tag, None, children, props)
 
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Tag value could not be empty")
+        if self.children is None:
+            raise ValueError("Children value could not be empty")
+        return f"<{self.tag}>{"".join(child.to_html() for child in self.children)}</{self.tag}>"
---
+++ src/test_parentnode.py
@@ -0,0 +1,27 @@
+import unittest
+from htmlnode import ParentNode, LeafNode
+
+class TestParentNode(unittest.TestCase):
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+        
+    def test_to_html_without_children(self):
+        parent_node = ParentNode("p")
+        with self.assertRaises(ValueError):
+            parent_node.to_html()
+
+
+if __name__ == "__main__":
+    unittest.main()
